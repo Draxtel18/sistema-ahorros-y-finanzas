2a9659a5f1c6289e7db877ceec3d118e53fa515a
Geuseppe Lara <geuseppe18@gmail.com>
2025-04-09 13:27:49 -0500

    Variables de entorno

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vcuymcmkjstdwfvblzfm.supabase.co';
-const supabaseAnonkey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjdXltY21ranN0ZHdmdmJsemZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg3NTg1NTEsImV4cCI6MjA0NDMzNDU1MX0.AsQTJJr0k0pYJlur0gkdrmei6Om15wPUSN6txbWdP-o';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonkey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonkey);
